868838f15cac332d635e581e28a0b4c810d8dd1e
Gador Cruz <gadorcruz@hotmail.com>
2023-02-28 20:44:53 +0000

    create messages controller basic actions

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,23 @@
 class MessagesController < ApplicationController
+  before_action :set_plan, only: %i[new create]
   def new
+    @message = Message.new
   end
 
   def create
+    @message = Message.new(messages_params)
+    @message.plan = @plan
+    @message.save
+    # redirect_to plan_path(@plan)
+  end
+
+  private
+
+  def set_plan
+    @plan = Plan.find(params[:plan_id])
+  end
+
+  def messages_params
+    params.require(:message).permit(:content)
   end
 end
