9ab74cc77971e1b7edc85b728153a0f317db68f4
Gador Cruz <gadorcruz@hotmail.com>
2023-02-28 19:52:20 +0000

    create basic plans controller actions

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,11 +1,19 @@
 class PlansController < ApplicationController
+  before_action :set_plan, only: %i[show edit update destroy]
   def index
-  end
-
-  def create
+    @plans = Plan.all
   end
 
   def new
+    @plan = Plan.new
+  end
+
+  def create
+    @plan = Plan.new(plan_params)
+    # @plan.planner_id = current_user
+    @plan.save!
+    # redirect_to plan_path(@plan)
+
   end
 
   def edit
@@ -15,8 +23,22 @@ class PlansController < ApplicationController
   end
 
   def update
+    @plan.update(plan_params)
+    # redirect_to plan_path(@plan)
   end
 
   def destroy
+    @plan.destroy
+    redirect_to plans_path, status: :see_other
+  end
+
+  private
+
+  def set_plan
+    @plan = Plan.find(params[:id])
+  end
+
+  def plan_params
+    params.require(:plan).permit(:title, :location, :cost, :description)
   end
 end
