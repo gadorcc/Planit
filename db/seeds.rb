617295f7b12c3c7639b6898759eb7585e1a6166c
Gador Cruz <gadorcruz@hotmail.com>
2023-03-05 18:32:34 +0000

    done seed of user but not avatar picture

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "open-uri"
+require 'faker'
+
+i = 0
+10.times do
+  i += 1
+  puts "Creating user #{i}"
+  User.create(
+    nickname: Faker::Name.female_first_name,
+    email: Faker::Internet.email,
+    password: '123456',
+  )
+end
