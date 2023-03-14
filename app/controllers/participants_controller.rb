da8f921fd51ec2f244b24b5279c005f7b71bab1b
Gador Cruz <gadorcruz@hotmail.com>
2023-03-14 19:29:36 +0000

    heroku push

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -18,16 +18,14 @@ class ParticipantsController < ApplicationController
     @participant.user = current_user
     @participant.update!(participant_params)
     redirect_to plan_path(@participant.plan)
-
   end
 
 
   private
 
-
-  def user_params		   def user_params
-    params.require(:user).permit(:nickname)		     params.require(:user).permit(:nickname)
-  end		   end
+  def user_params
+    params.require(:user).permit(:nickname)
+  end
 
   def participant_params
     params.require(:participant).permit(:status)
