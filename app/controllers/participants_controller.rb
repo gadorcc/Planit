6d2f1ce77564c280b11cc1c2608fa1ca98a5800d
Iain Tidbury <iain@mainpages.online>
2023-03-14 20:18:19 +0000

    Stylings resolved for cards on plans

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,5 +1,4 @@
 class ParticipantsController < ApplicationController
-
   def create
     @plan = Plan.find(params[:plan_id])
     @participant = Participant.new
@@ -18,16 +17,13 @@ class ParticipantsController < ApplicationController
     @participant.user = current_user
     @participant.update!(participant_params)
     redirect_to plan_path(@participant.plan)
-
   end
-
 
   private
 
-
-  def user_params		   def user_params
-    params.require(:user).permit(:nickname)		     params.require(:user).permit(:nickname)
-  end		   end
+  def user_params
+    params.require(:user).permit(:nickname)
+  end
 
   def participant_params
     params.require(:participant).permit(:status)
